8bc1f1bcb757f0b9bad10c080ecec347cea78710
RapidShotzz <arjbedi05@icloud.com>
2024-03-12 16:42:12 +0000

    Added the image to the frame class and changed the colour
    I added an image to the frame class which displays a map of the boroughs in London. Additionally, I modified the hex code of the design.css class so that it is completely white.

--- src/Frame.java
+++ src/Frame.java
@@ -20,6 +20,8 @@ import javafx.scene.text.Text;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.Pane;
+import java.io.File;
+import javafx.beans.binding.Bindings;
 
 /**
  * Write a description of class Frame here.
@@ -27,63 +29,70 @@ import javafx.scene.layout.Pane;
  * @author (your name)
  * @version (a version number or a date)
  */
-public class Frame extends Application
-{
- 
-    /**
-     * Constructor for objects of class Frame
-     */
-    public Frame()
-    {
-        
-    }
-     public static void main(String args[]){           
-        launch(args);      
-    } 
-    
+import javafx.geometry.Pos; // Import Pos for alignment
+
+import javafx.geometry.Pos;
+import javafx.scene.layout.Region;
+
+import javafx.geometry.Pos;
+
+public class Frame extends Application {
+
+    private ImageView mapView;
+
     @Override
     public void start(Stage stage) throws Exception {
         DateSelector dateSelector = new DateSelector();
         BorderPane root = new BorderPane();
-   
-        HBox topPart = dateSelector.createTopPart();    // where the date selection is located
-        Pane middlePart = new Pane(); // where the main map is located
-        HBox lowerPart = new HBox();  // where the buttons are to change the panels
-         
-        //set the IDs of each part so they can be modified in CSS file
+
+        HBox topPart = dateSelector.createTopPart();
+        Region leftPart = new Region(); // Using a Region for flexible spacing
+        Region rightPart = new Region(); // Using a Region for flexible spacing
+        HBox lowerPart = new HBox();
+
+        // Load the map image
+        Image mapImage = new Image(new File("/Users/arjanbedi/Documents/GitHub/CourseWork-4-COVID-19/src/boroughs.png").toURI().toString());
+        mapView = new ImageView(mapImage);
+
+        // Set up preserving aspect ratio
+        mapView.setPreserveRatio(true);
+
+        // Create a VBox for the middle part
+        VBox middlePart = new VBox();
+        middlePart.setAlignment(Pos.CENTER);
+
+        // Bind the fit width or height based on the image's aspect ratio
+        mapView.fitWidthProperty().bind(Bindings.min(middlePart.widthProperty(), middlePart.heightProperty()));
+        mapView.fitHeightProperty().bind(Bindings.min(middlePart.widthProperty(), middlePart.heightProperty()));
+
+        // Add the map image to the VBox
+        middlePart.getChildren().add(mapView);
+
+        // Set the IDs of each part for styling
         topPart.setId("topPart");
         middlePart.setId("middlePart");
         lowerPart.setId("lowerPart");
-        
-        //Text textLabel = new Text("Top Bar");
-        Text textLabelLow = new Text("Lower Bar");
-        Text textLabelMid = new Text("Middle Bar");
 
-        Scene scene = new Scene(root, 600,600, Color.WHITE);
+        // Load CSS
+        Scene scene = new Scene(root, 600, 600, Color.WHITE);
         scene.getStylesheets().add("design.css");
 
-        //topPart.getChildren().addAll(textLabel);
-        middlePart.getChildren().addAll(textLabelMid); // Add content to middlePart
-        lowerPart.getChildren().addAll(textLabelLow);
-
+        // Set preferred heights for parts
         topPart.setPrefHeight(50);
-        middlePart.setPrefHeight((double)(scene.getHeight()/(2)));
         lowerPart.setPrefHeight(50);
-      
+
+        // Add parts to the root
         root.setTop(topPart);
+        root.setLeft(leftPart);
         root.setCenter(middlePart);
+        root.setRight(rightPart);
         root.setBottom(lowerPart);
 
         stage.setTitle("Template Structure");
         stage.setScene(scene);
         stage.show();
-        
     }
+}
 
-    }
-    
-    
-    
 
-    
 
